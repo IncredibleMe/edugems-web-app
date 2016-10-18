9e83a69ae205941a1e180c47ec8c8b1f121419ef
rambou <nickos.bou@gmail.com>
2016-10-18 03:25:55 +0300

    Added register component

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {ApiService} from "../api.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(public router: Router, private api: ApiService) {
+  }
+
+  ngOnInit() {
+  }
+
+  register(form: any): void {
+    console.log('Form Data: ');
+    console.log(form);
+    this.api.register(form).subscribe(
+      (result) => {
+        if (result) {
+          this.router.navigate(['home']);
+        }
+      },
+      error => {
+        alert(error.text());
+        console.log(error.text());
+      });
+  }
+
+}
