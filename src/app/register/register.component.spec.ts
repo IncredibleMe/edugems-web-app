bb1934b8f12d74dda04b0871696385cfcf2a747a
rambou <nickos.bou@gmail.com>
2016-10-18 03:31:03 +0300

    Minor fix on register testing component

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -2,10 +2,14 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { RegisterComponent } from './register.component';
+import {ApiService} from "../api.service";
+import {Router} from "@angular/router";
 
 describe('Component: Register', () => {
   it('should create an instance', () => {
-    let component = new RegisterComponent();
+    let router: Router;
+    let api: ApiService;
+    let component = new RegisterComponent(router, api);
     expect(component).toBeTruthy();
   });
 });
