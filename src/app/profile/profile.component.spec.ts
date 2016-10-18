adce7baa98b6bc6cc0610ea0dc175aea6cedc24d
rambou <nickos.bou@gmail.com>
2016-10-18 04:02:42 +0300

    Added profile component

---
+++ src/app/profile/profile.component.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import {ProfileComponent} from "./profile.component";
+
+describe('Component: Profile', () => {
+  it('should create an instance', () => {
+    let component = new ProfileComponent();
+    expect(component).toBeTruthy();
+  });
+});
