098dfece94c43664ad303e68eb78d1be921c206f
Incredibleme <alfa@aegean.gr>
2016-10-27 18:21:21 +0300

    register validators almost there

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
 import {LoginComponent} from "./login/login.component";
@@ -36,7 +36,8 @@ import {ApiService} from "./api.service";
       {path: 'myprofile', component: ProfileComponent, canActivate: [LoggedInGuard]},
       {path: '', component: HomeComponent},
       {path: '**', component: PageNotFoundComponent}
-    ])
+    ]),
+    ReactiveFormsModule
   ],
   providers: [ApiService, LoggedInGuard],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ElementRef} from "@angular/core";
 import {Router} from "@angular/router";
 import {ApiService} from "../api.service";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,10 +10,21 @@ import {ApiService} from "../api.service";
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(public router: Router, private api: ApiService, private el: ElementRef) {
+  regForm:FormGroup;
+
+  constructor(private fb:FormBuilder, public router: Router, private api: ApiService, private el: ElementRef) {
+    this.regForm = fb.group({
+      // We can set default values by passing in the corresponding value or leave blank if we wish to not set the value. For our example, we’ll default the gender to female.
+      'username' : '',
+      'password': '',
+      'email' : '',
+      'name' : '',
+      'surname' : ''
+    })
   }
 
   ngOnInit() {
+    this.buildForm();
   }
 
   register(form: any): void {
@@ -33,5 +45,47 @@ export class RegisterComponent implements OnInit {
         snackbarContainer.MaterialSnackbar.showSnackbar(data);
       });
   }
+
+  buildForm(): void {
+    this.regForm = this.fb.group({
+      'username': [null, [Validators.required, Validators.minLength(4), Validators.maxLength(24)]],
+      'password': [null, [Validators.required, Validators.minLength(4), Validators.maxLength(24)]],
+      'email': [null, [Validators.required, Validators.minLength(4), Validators.maxLength(24)]],
+      'name': [null, [Validators.required, Validators.minLength(4), Validators.maxLength(24)]],
+      'surname': [null, [Validators.required, Validators.minLength(4), Validators.maxLength(24)]],
+
+    });
+    this.regForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.regForm) { return; }
+    const form = this.regForm;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  formErrors = {
+    'username': ''
+  };
+
+  validationMessages = {
+    'username': {
+      'required': 'username is required.'
+    }
+  };
 
 }
