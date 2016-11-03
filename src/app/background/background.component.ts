9652f9e61489a5f1c40dc1627af495d678cf4313
rambou <nickos.bou@gmail.com>
2016-11-03 04:09:22 +0200

    Added background component

---
+++ src/app/background/background.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit, ViewEncapsulation} from "@angular/core";
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class BackgroundComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
